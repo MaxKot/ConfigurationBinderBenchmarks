fcd208511a0ab0408c75c60e4425b817a43d3e92
Max A. Kiselev <kiselevma@gmail.com>
2023-01-21 18:55:23 +0300

    add empty project

---
+++ src/ConfigurationBinderBenchmarks/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConfigurationBinderBenchmarks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
