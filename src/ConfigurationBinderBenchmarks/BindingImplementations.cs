75fb74843e71825c9f286f71c964d26c0d5a8168
Max A. Kiselev <kiselevma@gmail.com>
2023-01-21 22:26:48 +0300

    add implementations to compare

--- src/ConfigurationBinderBenchmarks/BindingImplementations.cs
+++ src/ConfigurationBinderBenchmarks/BindingImplementations.cs
@@ -40,5 +40,70 @@ namespace ConfigurationBinderBenchmarks
 
             return result;
         }
+
+        [Benchmark]
+        public object? LazyReflection()
+        {
+            var result = new object?[Iterations];
+            for (var i = 0; i < Iterations; ++i)
+            {
+                result[i] = ConfigurationBinder.BindDictionaryInterface_LazyReflection
+                    (_source, typeof(IDictionary<string, int>), null!, null!);
+            }
+
+            return result;
+        }
+
+        [Benchmark]
+        public object? AddKvp()
+        {
+            var result = new object?[Iterations];
+            for (var i = 0; i < Iterations; ++i)
+            {
+                result[i] = ConfigurationBinder.BindDictionaryInterface_AddKvp
+                    (_source, typeof(IDictionary<string, int>), null!, null!);
+            }
+
+            return result;
+        }
+
+        [Benchmark]
+        public object? Ctor()
+        {
+            var result = new object?[Iterations];
+            for (var i = 0; i < Iterations; ++i)
+            {
+                result[i] = ConfigurationBinder.BindDictionaryInterface_Ctor
+                    (_source, typeof(IDictionary<string, int>), null!, null!);
+            }
+
+            return result;
+        }
+
+        [Benchmark]
+        public object? MakeMethod()
+        {
+            var result = new object?[Iterations];
+            for (var i = 0; i < Iterations; ++i)
+            {
+                result[i] = ConfigurationBinder.BindDictionaryInterface_MakeMethod
+                    (_source, typeof(IDictionary<string, int>), null!, null!);
+            }
+
+            return result;
+        }
+
+        [Benchmark]
+        public object? Factory()
+        {
+            var result = new object?[Iterations];
+            for (var i = 0; i < Iterations; ++i)
+            {
+                result[i] = ConfigurationBinder.BindDictionaryInterface_Factory
+                    (_source, typeof(IDictionary<string, int>), null!, null!);
+            }
+
+            return result;
+        }
     }
 }
